8b8e1c55878d336fadfb9d4280489624a6fe1c83
rion <rion.z924@gmail.com>
2022-04-25 15:50:00 +0800

    perf():优化代码[]

--- src/store/modules/system/index.ts
+++ src/store/modules/system/index.ts
@@ -10,12 +10,12 @@ export default defineStore('system', {
   actions: {
     changeTheme() {
       this.theme = this.theme === 'light' ? 'dark' : 'light'
-      const userTheme = style[this.theme]
+      const userTheme: Color = style[this.theme]
       const root = document.documentElement
       for (let i in userTheme) {
         const item: any = userTheme[i as keyof Color]
         for (let j in item) {
-          let cssVarName =
+          const cssVarName =
             '--ry-' + i + '-' + j.replace(/([A-Z])/g, '-$1').toLowerCase()
           root.style.setProperty(cssVarName, item[j])
         }
