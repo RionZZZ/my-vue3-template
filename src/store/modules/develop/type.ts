5d122a75fff2bdb349c160bed05fce583d8db5c6
Rion <rion.z924@gmail.com>
2023-04-08 09:37:11 +0800

    optimizate

--- src/store/modules/develop/type.ts
+++ src/store/modules/develop/type.ts
@@ -3,5 +3,5 @@ import { Relation, Data, BusinessForm } from '@/views/develop/type'
 export interface Develop {
   relation: Relation
   data: Data
-  business: { form: BusinessForm; module: BusinessForm }
+  business: { form: BusinessForm, module: BusinessForm }
 }
